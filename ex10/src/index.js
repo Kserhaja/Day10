2fcf333063fa114746e233145fff37e85a40d378
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-17 23:29:14 +0100

    the beatles and led zeppelin

---
+++ ex10/src/index.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var myMusic = {
+    001: {
+        artist: "The Beatles",
+        title: "Get Back",
+        release_year: 1970,
+        formats: {
+            1: "LP",
+            2: "CD",
+            3: "Cassete tape"
+        },
+        gold: true
+    },
+    002: {
+        artist: "Led Zeppelin",
+        title: "Whole Lotta Love",
+        release_year: 1969,
+        formats: {
+            1: "LP",
+            2: "CD",
+            3: "Cassete tape"
+            },
+        },
+
+    };
+
+return myMusic;
+}
+myFunction()[1];
+console.log(myFunction()[1]);
+module.exports = myFunction;
