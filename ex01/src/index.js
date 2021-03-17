631c52a4715d663abd2f34214e3a362fe83109b4
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-17 19:56:09 +0100

    Dog Lexter with 4 legs

---
+++ ex01/src/index.js
@@ -0,0 +1,28 @@
+
+    
+    var myPet = {
+        species: 'Dog',
+        name: 'Lexter',
+        legs: 4,
+        Friends: [ 'Liso', 'Jacky' ]   
+      }
+
+
+    
+
+    // Only change code bellow this line
+    // Only change code above this line
+    
+    function myFunction(myPet) {
+        
+        return myPet;
+
+
+    }
+
+   
+
+myFunction ()
+console.log(myFunction(myPet));
+
+module.exports = {myPet, myFunction};
